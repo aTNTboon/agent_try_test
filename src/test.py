d13623c9cc7e5c81c5347b6a8d757391b8856006
algernon <92218838+aTNTboon@users.noreply.github.com>
2026-04-02 11:47:35 +0800

    feat: add category selector and chain test integration

--- src/test.py
+++ src/test.py
@@ -1,15 +1,21 @@
-import src.util.ToolSelecrot.MeetingToolSelector as sqlTool
+import src.util.ToolSelecrot.CatagorySelector as category_selector_module
+import src.util.ToolSelecrot.MeetingToolSelector as tool_selector_module
 
 
-tool_selector = sqlTool.DefaultToolSelector()
-tools:list[int] = tool_selector.query({
-    "question":"我昨天有没有没参加的会议",
-    "category":["meeting"]
-})
-print(tools)
+category_selector = category_selector_module.CatagoryToolSelector()
+selected_categories = category_selector.query(
+    {
+        "question": "我昨天有没有没参加的会议",
+    }
+)
+print("selected_categories:", selected_categories)
 
 
-# {"question":"我昨天有没有没参加的会议"}|{tool_selector|}
-
-
-
+tool_selector = tool_selector_module.DefaultToolSelector()
+selected_tools: list[int] = tool_selector.query(
+    {
+        "question": "我昨天有没有没参加的会议",
+        "category": selected_categories,
+    }
+)
+print("selected_tools:", selected_tools)
